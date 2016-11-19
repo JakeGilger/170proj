a033724c35f3f62099129374cb30df0ffaba4c96
Jake Gilger <jake_giggles@Jakes-MacBook-Air-2.local>
2016-11-18 18:29:59 -0800

    updated creation script

--- generate_input.py
+++ generate_input.py
@@ -1,15 +1,25 @@
 import random
 
+#Put the reciprocal of the odds into odds (i.e. 1/10: odds = 10)
+odds = 10
+vertex_count = 64
+file_number = 2
+
+
+
+sample_list = [0] * (odds - 1)
+sample_list.append(1)
+
 matrix = ""
-for i in range(32):
+for i in range(vertex_count):
 	line = ""
-	for j in range(32):
+	for j in range(vertex_count):
 		if i == j:
 			line += str(random.randint(1, 99)) + " "
 		else:
-			line += str(random.sample((0, 0, 1), 1)[0]) + " "
+			line += str(random.sample(sample_list, 1)[0]) + " "
 	line.rstrip()
 	line += "\n"
 	matrix += line
-with open("evil.txt", "w") as f:
-	f.write(matrix)
+with open(str(file_number) + ".in", "w") as f:
+	f.write(str(vertex_count) + "\n" + matrix)
