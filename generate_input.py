79d8e7c2395d6ed808c0285bd45b6cb4ad6564fe
Derek Pierce <derekpierce@tests-MacBook-Pro-5.local>
2016-11-21 14:35:50 -0800

    all zeroes with some number of paths

--- generate_input.py
+++ generate_input.py
@@ -2,14 +2,16 @@ import random
 
 #Put the reciprocal of the odds into odds (i.e. 1/10: odds = 10)
 odds = 10
-vertex_count = 20
-file_number = 2
+all_zeroes = 0
+vertex_count = 256
+file_number = 3
 
 #Set the following value to true if you wish to force a Hamiltonian Path in the graph.
 force_hamiltonian_path = True
+num_paths = 6
 
 sample_list = [0] * (odds - 1)
-sample_list.append(1)
+sample_list.append(all_zeroes)
 
 def writeline():
 	line = []
@@ -22,11 +24,12 @@ for j in range(vertex_count):
 	matrix.append(writeline())
 
 if (force_hamiltonian_path):
-	path = range(0, vertex_count)
-	random.shuffle(path)
-	print("The path through this graph is: " + str(path))
-	for i in range(0, vertex_count - 1):
-		matrix[path[i]][path[i+1]] = 1
+	for a in range(num_paths):
+		path = range(0, vertex_count)
+		random.shuffle(path)
+		print("The path through this graph is: " + str(path))
+		for i in range(0, vertex_count - 1):
+			matrix[path[i]][path[i+1]] = 1
 		
 
 matrix_string = ""
