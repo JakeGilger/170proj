c3e9459e0dba41bc890176eef73861afaedd10ab
Jake Gilger <jake_giggles@Jakes-MacBook-Air-3.local>
2016-12-02 02:54:21 -0800

    output changed

--- combine.py
+++ combine.py
@@ -7,6 +7,7 @@ def combine_outputs(output1, output2):
 		f2lines = f.readlines()
 	score1sum = 0
 	score2sum = 0
+	neithersum = 0
 	with open("combined_output.out", "w") as f:
 		for i in range(1, len(f1lines) + 1):
 			score1 = score_from_file(i, f1lines)
@@ -15,9 +16,12 @@ def combine_outputs(output1, output2):
 				score1sum += 1
 				f.write(f1lines[i - 1])
 			else:
-				score2sum += 1
+				if score1[0] == score2[0]:
+					neithersum += 1
+				else:
+					score2sum += 1
 				f.write(f2lines[i - 1])
-	print(str(score1sum) + ", " + str(score2sum))
+	print(str(score1sum) + ", " + str(score2sum) + ", with " + str(neithersum) + " equal.")
 
 def score_from_file(line_number, file_lines):
 	perf = load_obj(str(line_number) + ".perf")
