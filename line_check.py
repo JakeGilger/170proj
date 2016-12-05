61cc305454356de20c3fef4ee37169c05b9c40d6
Jake Gilger <jake_giggles@airbears2-10-142-71-224.airbears2.1918.berkeley.edu>
2016-12-04 16:51:30 -0800

    line check is more verbose

--- line_check.py
+++ line_check.py
@@ -1,20 +1,33 @@
 import cPickle
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
 	print "Usage Example: python line_check.py exampoutput.out"
 	exit()
 
 
 OUTPUT_FILE_TO_CHECK = sys.argv[1]
-
+with open(OUTPUT_FILE_TO_CHECK) as f:
+	flines = f.readlines()
 
 def load_obj(name):
 	with open('obj/' + name, 'rb') as f:
 		return cPickle.load(f)
 
-with open(OUTPUT_FILE_TO_CHECK) as f:
-	flines = f.readlines()
+def score_from_line(line_number, line):
+	perf = load_obj(str(line_number) + ".perf")
+	orig_line = line
+	line = line.split(";")
+	num_teams = len(line)
+	total = 0
+	for i in line:
+		team = i.strip()
+		team = team.split(" ")
+		teamSum = 0
+		for j in team:
+			teamSum += perf[int(j)]
+		total += len(team) * teamSum
+	return (total, num_teams)
 
 def check_line(problem_number, output_line):
 	adj = load_obj(str(problem_number) + ".adj")
@@ -29,8 +42,12 @@ def check_line(problem_number, output_line):
 		for horse_posit in range(len(teams[team_num]) - 1):
 			if int(curr_team[horse_posit + 1]) not in adj[int(curr_team[horse_posit])]:
 				print("Horse at position: " + str(horse_posit + 1) + " in team: " + str(team_num + 1) + " has no edge to: " + str(curr_team[horse_posit + 1]))
+	score = score_from_line(problem_number, output_line)
+	if score[1] == 1:
+		print("Line: " + str(problem_number) + " solved.")
+	else:
+		print("Line: " + str(problem_number) + " " + str(score))
 
 for i in range(1, 601):
-	print("Line: " + str(i))
 	line = flines[i - 1]
 	check_line(i, line)
